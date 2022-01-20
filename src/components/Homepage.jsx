f892e8c21bb65ed776b0b5f41d2eceeb71c78631
Rohit <rohitc437>
2022-01-20 14:22:32 +0530

    All file updated

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,11 @@
+export const Homepage = () => {
+  return (
+    <>
+      <h1>Homepage</h1>
+      <img
+        src="https://line25.com/wp-content/uploads/2019/08/Header-image.jpg"
+        alt="homepage"
+      />
+    </>
+  );
+};
